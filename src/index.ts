1b81adecef04c4d5fc7a9eb1816082722421d0cc
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-06 17:35:21 -0400

    Added Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
 
 const app = new Hono()
+const prisma = new PrismaClient()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     name: "Alice",
+  //     email: "alice@prisma.io",
+  //     posts: { create: { title: "Hello World" }},
+  //     profile: { create: { bio: "I like turtles" }}
+  //   }
+  // })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true
+    }
+  })
+
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+app.get("/", (c) => c.text("Hello Hono!"))
 
 export default app
